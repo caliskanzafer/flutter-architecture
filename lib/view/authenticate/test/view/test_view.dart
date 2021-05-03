576236cb06c4cb6d1214bb8e2a27b33dd6b30154
Zafer Caliskan <caliskan.zafer@outlook.com>
2021-05-03 20:20:18 +0300

    mobx eklendi. state management ogreniyorum

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture/core/base/state/base_state.dart';
-import 'package:flutter_architecture/core/base/view/base_view.dart';
+import 'package:flutter_architecture/core/base/view/base_widget.dart';
+import 'package:flutter_architecture/view/authenticate/test/viewmodel/test_view_model.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class TestsView extends StatefulWidget {
   @override
@@ -8,11 +10,31 @@ class TestsView extends StatefulWidget {
 }
 
 class _TestsViewState extends BaseState<TestsView> {
+  TestViewModel viewModel;
   @override
   Widget build(BuildContext context) {
-    return BaseView(
-      viewModel: '',
+    return BaseView<TestViewModel>(
+      viewModel: TestViewModel(),
+      onModelReady: (model) {
+        viewModel = model;
+      },
       onPageBuilder: (context, value) => Text('data'),
     );
   }
+
+  Widget get scaffoldBody => Scaffold(
+        floatingActionButton: floatingActionButtonIncrement,
+        body: textNumber,
+      );
+
+  FloatingActionButton get floatingActionButtonIncrement =>
+      FloatingActionButton(
+        onPressed: () => viewModel.incrementNumber(),
+      );
+
+  Widget get textNumber => Observer(
+        builder: (context) => Text(
+          viewModel.number.toString(),
+        ),
+      );
 }
